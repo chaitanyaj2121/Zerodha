6cf9cc05500203439a20926b7b924abf46382c99
Chaitanya Jawanjal <chaitanyajawanjal20@gmail.com>
2025-10-21 23:01:51 +0530

    Done with the Pricing page

--- frontend/src/landing_page/pricing/PricingPage.jsx
+++ frontend/src/landing_page/pricing/PricingPage.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-
+import React from "react";
+import Hero from "./Hero";
+import Brokerage from "./Brokerage";
+import OpenAccount from "../OpenAccount";
 function PricingPage() {
-	return (
-		<main>
-			<h2>Pricing Page</h2>
-		</main>
-	);
+  return (
+    <>
+      <Hero></Hero>
+      <OpenAccount></OpenAccount>
+      <Brokerage></Brokerage>
+    </>
+  );
 }
 
 export default PricingPage;
