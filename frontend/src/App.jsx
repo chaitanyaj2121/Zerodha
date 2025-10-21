141b84c36fa91b6f05c29b9978f72f6a5daf627c
Chaitanya Jawanjal <chaitanyajawanjal20@gmail.com>
2025-10-21 20:36:58 +0530

    Created React Router Dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
-
-import './App.css'
-import HomePage from './landing_page/home/HomePage'
+import "./App.css";
+import HomePage from "./landing_page/home/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signup from "./landing_page/signup/Signup";
+import About from "./landing_page/about/AboutPage";
+import Product from "./landing_page/products/ProductsPage";
+import Pricing from "./landing_page/pricing/PricingPage";
+import Support from "./landing_page/support/SupportPage";
+import Navbar from "./landing_page/Navbar";
+import Footer from "./landing_page/Footer";
 function App() {
-
-
   return (
     <>
-    <HomePage/>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/support" element={<Support />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/landing_page/home/HomePage.jsx
+++ frontend/src/landing_page/home/HomePage.jsx
@@ -1,28 +1,23 @@
-import React from 'react';
-import Hero from '../home/Hero.jsx';
-import Education from '../home/Education.jsx';
-import Stats from '../home/Stats.jsx';
-import Pricing from '../home/Pricing.jsx';
-import Awards from '../home/Awards.jsx';
-import OpenAccount from '../OpenAccount.jsx';
-import Navbar from '../Navbar.jsx'
-import Footer from '../Footer.jsx';
+import React from "react";
+import Hero from "../home/Hero.jsx";
+import Education from "../home/Education.jsx";
+import Stats from "../home/Stats.jsx";
+import Pricing from "../home/Pricing.jsx";
+import Awards from "../home/Awards.jsx";
+import OpenAccount from "../OpenAccount.jsx";
+import Navbar from "../Navbar.jsx";
+import Footer from "../Footer.jsx";
 
 function HomePage() {
-    return ( <> 
-    <Navbar/>
-   <Hero/>
-   <Awards/>
-   <Stats/>
-   <Pricing/>
-    <Education/>
-    <OpenAccount/>
-    <Footer/>
-    
-
-    
-    
-    
-    </>);
+  return (
+    <>
+      <Hero />
+      <Awards />
+      <Stats />
+      <Pricing />
+      <Education />
+      <OpenAccount />
+    </>
+  );
 }
 export default HomePage;
