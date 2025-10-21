1045d69d8ad53ff4537501ff85c895721d4b4ec3
Chaitanya Jawanjal <chaitanyajawanjal20@gmail.com>
2025-10-21 11:35:31 +0530

    created hero component

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-function Education() {
+function Hero() {
     return ( <>
-    <h3>
-       Education Component
-    </h3>
+        <div className="container p-5 ">
+            <div className="row text-center ">
+                <img src="./media/images/homeHero.png"  className='mb-5' alt="home Hero image" />
+                <h1 className='mt-5'>Invest in everything</h1>
+                <p className='m-2'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button style={{width:"20%",margin:"0 auto", fontWeight:"500"}} className="btn btn-primary p-2 mt-4">Signup For Free</button>
+            </div>
+        </div>
     </>);
 }
-export default Education;
+export default Hero;
