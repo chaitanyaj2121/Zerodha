c019757ad03b71c331a10597de2f197ebbcc41c5
Chaitanya Jawanjal <chaitanyajawanjal20@gmail.com>
2025-10-23 07:16:06 +0530

    Creaded schemas and models

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+const uri = process.env.MONGO_URL;
+
+app.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
+  mongoose.connect(uri);
+  console.log("Connected to MongoDB");
 });
