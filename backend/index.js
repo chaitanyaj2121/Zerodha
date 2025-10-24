e6a18ca391b7de17209cf34498faa6d90aae5120
Chaitanya Jawanjal <chaitanyajawanjal20@gmail.com>
2025-10-24 08:52:07 +0530

    Success fetched the data from the backend and showed in the frontend holdings.jsx

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,38 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 const uri = process.env.MONGO_URL;
+
+const HoldingsModel = require("./model/HoldingsModel");
+const { PositionsModel } = require("./model/PositionsModel");
+
+app.get("/allHoldings", async (req, res) => {
+  let allHoldings = await mongoose.connection.db
+    .collection("holdings")
+    .find({})
+    .toArray();
+
+  // let allHoldings = await HoldingsModel.find({});  // Alternative using Mongoose model
+
+  res.json(allHoldings);
+});
+
+app.get("/allPositions", async (req, res) => {
+  const allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+});
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -1,9 +1,19 @@
-import React from "react";
-import { holdings } from "../data/Data";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 const Holdings = () => {
+  const [allHoldings, setAllHoldings] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/allHoldings").then((response) => {
+      console.log(response.data);
+      setAllHoldings(response.data);
+    });
+  }, []);
+
   return (
     <>
-      <h3 className="title">Holdings ({holdings.length})</h3>
+      <h3 className="title">Holdings ({allHoldings.length})</h3>
 
       <div className="order-table">
         <table>
@@ -17,7 +27,7 @@ const Holdings = () => {
             <th>Net chg.</th>
             <th>Day chg.</th>
           </tr>
-          {holdings.map((stock, index) => {
+          {allHoldings.map((stock, index) => {
             const curValue = stock.price * stock.qty;
             const isProfit = curValue - stock.avg * stock.qty >= 0.0;
             const profClass = isProfit ? "profit" : "loss";
@@ -25,7 +35,7 @@ const Holdings = () => {
             return (
               <tr key={index}>
                 <td>{stock.name}</td>
-                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
                 <td>{stock.avg.toFixed(2)}</td>
                 <td>{stock.price.toFixed(2)}</td>
                 <td>{curValue.toFixed(2)}</td>
